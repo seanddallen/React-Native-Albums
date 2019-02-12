06babfbcb4b35b7e623c76de992b61231643b97a
Sean Tayler <seanjtayler@gmail.com>
2019-02-12 12:27:34 -0700

    added axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,16 @@
 /* eslint-disable eol-last */
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+    render(){
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList; 

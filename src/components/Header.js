12c8a84ff846d0bb68acffe6faa0a4faf60f91b7
Sean Tayler <seanjtayler@gmail.com>
2019-02-11 21:26:36 -0700

    setup header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable semi */
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => {
+    return <Text>Albums</Text>
+};
+
+export default Header; 
+
